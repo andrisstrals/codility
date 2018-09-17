61c850a430e19c631baa47454a96c019330a3cf7
Andris Strals <andris.strals@gmail.com>
2018-09-17 15:06:55 +0100

    Unit test for slices longer than 2 elements

--- src/test/java/TestMinAvgTwoSlice.java
+++ src/test/java/TestMinAvgTwoSlice.java
@@ -1,7 +1,12 @@
 import com.MinAvgTwoSlice;
 import org.junit.Test;
 
+import java.util.Random;
+import java.util.stream.IntStream;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 
 public class TestMinAvgTwoSlice {
     private MinAvgTwoSlice sol = new MinAvgTwoSlice();
@@ -9,12 +14,23 @@ public class TestMinAvgTwoSlice {
     @Test
     public void shouldFindMinofSliceLen2() {
         int[] in = {4, 2, 2, 5, 1, 5, 8};
-        assertEquals(1, sol.solution(in) );
+        assertEquals(1, sol.solution(in));
     }
 
     @Test
     public void shouldFindMinofSliceLen3() {
         int[] in = {4, 2, -10, -4, -8, 8};
-        assertEquals(2, sol.solution(in) );
+        assertEquals(2, sol.solution(in));
+    }
+
+    @Test
+    public void timingTest() {
+        Random r = new Random();
+        int[] in = IntStream.generate(r::nextInt).limit(10000000).toArray();
+        long start = System.currentTimeMillis();
+        sol.solution(in);
+        long time = System.currentTimeMillis() - start;
+        System.out.println("in " + in.length + " values, time:" + time);
+        assertTrue(time < 1000);
     }
 }
